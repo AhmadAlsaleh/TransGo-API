b14d969e677449e31cd99091ad883ba3e68619ba
Ahmad <alsaleh.a.ahmad@gmail.com>
2018-11-18 08:37:25 +0200

    1

--- models/globals.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    localDBconnectionString : "mongodb://127.0.0.1:27017/trans_go",
-    remoteDBconnectionString : "mongodb://ahmad_trans_go:A16248Ba@ds155653.mlab.com:55653/trans_go"
-}
